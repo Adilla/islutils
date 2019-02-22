3906fa904c3071fe576761c2e3bd41ce7a1d5860
Adilla <adilla.susungi@gmail.com>
2019-02-22 14:17:54 +0100

    minor change: reorganised code

--- test/blas_tree_matchers.h
+++ test/blas_tree_matchers.h
@@ -6,54 +6,6 @@ using namespace matchers;
 
 namespace blasTreeMatchers {
 
-bool findGemmTree(isl::schedule_node root, isl::schedule_node *node) {
-
-	// For the moment reusing what is already implemented in test_transformer.cc
-	// This is also valid for transposeGemm
-	//isl::schedule_node node;
-	auto matcher = band(
-			[&node](isl::schedule_node n) {
-				if (isl_schedule_node_band_n_member(n.get()) < 3) {
-					return false;
-				} else {
-					node = &n;
-					return true;
-				}
-			}, leaf());
-	auto res = ScheduleNodeMatcher::isMatching(matcher, root.child(0));
-	return res;
-}
-
-
-bool findBatchedGemmTree(isl::schedule_node root, isl::schedule_node *node) {
-	//isl::schedule_node node;
-	// This is also valid for transposed Batched 
-	auto matcher = band(
-			[&node](isl::schedule_node n) {
-				if (isl_schedule_node_band_n_member(n.get()) < 4) {
-					return false;
-				} else {
-					node = &n;
-					return true;
-				}
-			}, leaf());
-	auto res = ScheduleNodeMatcher::isMatching(matcher, root.child(0));
-	return res;
-}
-
-bool findTransposeTree(isl::schedule_node root, isl::schedule_node *node) {
-	auto matcher = band(
-		[&node] (isl::schedule_node n) {
-			if (isl_schedule_node_band_n_member(n.get()) < 2) {
-				return false;
-			} else {
-				node = &n;
-				return true;
-			}
-		}, leaf());
-		auto res = ScheduleNodeMatcher::isMatching(matcher, root.child(0));
-		return res;
-}
 
 bool find1DTree(isl::schedule_node root) {
 	// Ensure if u need to check if strided or not.
@@ -62,6 +14,34 @@ bool find1DTree(isl::schedule_node root) {
 								);
 	return ScheduleNodeMatcher::isMatching(matcher, root.child(0));
 } 
+
+bool findNDPermutableBand(isl::schedule_node root, isl::schedule_node *node, int nbDim) {
+	// For the moment reusing what is already implemented in test_transformer.cc
+	// This is also valid for transposeGemm
+	auto matcher = band(
+			[&node, nbDim](isl::schedule_node n) {
+				if (isl_schedule_node_band_n_member(n.get()) < nbDim) {
+					return false;
+				} else {
+					node = &n;
+					return true;
+				}
+			}, leaf());
+	return ScheduleNodeMatcher::isMatching(matcher, root.child(0));
+}
+
+bool findGemmTree(isl::schedule_node root, isl::schedule_node *node) {
+	return findNDPermutableBand(root, node, 3);
+}
+
+
+bool findBatchedGemmTree(isl::schedule_node root, isl::schedule_node *node) {
+	return findNDPermutableBand(root, node, 4);
+}
+
+bool findTransposeTree(isl::schedule_node root, isl::schedule_node *node) {
+	return findNDPermutableBand(root, node, 2);
+}
 
 bool findAxpyTree(isl::schedule_node root, isl::schedule_node *node) {
 	return find1DTree(root);
